b86a3c3e7dfd875cb7089cb3e5364401df13bd9d
Ali Ayaz <160322443+Ali27Ayaz@users.noreply.github.com>
2024-02-24 13:57:04 +0500

    Add files via upload

---
+++ class1.js
@@ -0,0 +1,14 @@
+"use strict";
+// Asslam o Alaikum
+console.log();
+console.log(" Hi This is Ali Ayaz");
+console.log();
+console.log(" This is Hello World or You may call it its A.I world");
+console.log();
+console.log(" In this fastest growing world you have to pick up yourself fast as per world");
+console.log();
+console.log(" Other wise theres no future for You . so i am here in kind Circle of");
+console.log();
+console.log(" Dearest Sir, Kamran Khan Tessori sb,  and best persnality Sir Zia Khan & its team");
+console.log();
+console.log("Its my first Class of Typescript & I am enjoying it");
